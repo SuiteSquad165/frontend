ba518542721ac830eef8933bb98fdfd8c219bcce
Nathan Dinh <nathanxdinhx97@gmail.com>
2024-10-16 00:37:40 -0700

    update: add user login image

--- src/components/navbar/UserIcon.tsx
+++ src/components/navbar/UserIcon.tsx
@@ -1,7 +1,27 @@
+import { RootState } from "@/store";
 import { LuUser2 } from "react-icons/lu";
+import { useSelector } from "react-redux";
+import Image from "next/image";
 
 const UserIcon = () => {
-  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
+  const user = useSelector((state: RootState) => state.auth.user);
+  console.log("Hello");
+  return (
+    <>
+      {user?.photoURL ? (
+        <div className="relative rounded-full w-6 h-6 overflow-hidden">
+          <Image
+            src={user.photoURL || "No avatar"}
+            fill
+            alt={user.displayName || "No avatar"}
+            className="rounded-full object-cover"
+          />
+        </div>
+      ) : (
+        <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />
+      )}
+    </>
+  );
 };
 
 export default UserIcon;
