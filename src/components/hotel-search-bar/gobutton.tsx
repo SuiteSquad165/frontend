8b33ed56c63abac79e9b77a1d8cf2b5a6e8183a2
Nathan Dinh <nathanxdinhx97@gmail.com>
2024-11-19 22:11:17 -0800

    fix: invalid DOM properties and add missing lockfile

--- src/components/hotel-search-bar/gobutton.tsx
+++ src/components/hotel-search-bar/gobutton.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
 
 export default function GoButton() {
-    return (
-        <Link href={`/search`}>
-            <button className="rounded-full border bg-blue-600 p-4 text-white hover:bg-blue-700">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
-            </button>
-        </Link>
-    );
+  return (
+    <Link href={`/search`}>
+      <button className="rounded-full border bg-blue-600 p-4 text-white hover:bg-blue-700">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+      </button>
+    </Link>
+  );
 }
